ce9c23cafeb487ee22ad791f5898852e8d0d3638
rajuahmed118124 <rajuahmed118124@gmail.com>
2024-12-01 12:17:28 +0600

    "3rd Commit"

--- Scenario alpha/GeographicalAnomalyCheck.java
+++ Scenario alpha/GeographicalAnomalyCheck.java
@@ -3,22 +3,28 @@ package Scenario alpha;
 import java.util.Arrays;
 import java.util.List;
 
-public class GeographicalAnomalyCheck implements FraudCheck {
-    private FraudCheck nextCheck;
-    private List<String> validLocations = Arrays.asList("USA", "Canada", "UK");
+public class GeographicalAnomalyCheck implements Fraud_Check 
+{
+    private Fraud_Check next_Check;
+    private List<String> validLocations = Arrays.asList("BAN", "INDIA", "Pakistan");
 
     @Override
-    public void setNextCheck(FraudCheck nextCheck) {
-        this.nextCheck = nextCheck;
+    public void setnext_Check(Fraud_Check next_Check) 
+    {
+        this.next_Check = next_Check;
     }
 
     @Override
-    public void check(Transaction transaction) {
-        if (!validLocations.contains(transaction.getLocation())) {
-            transaction.setFlagged(true);
-            System.out.println("Geographical Check Failed: Unusual location for " + transaction.getAccountId());
-        } else if (nextCheck != null) {
-            nextCheck.check(transaction);
+    public void check(Transaction transaction) 
+    {
+        if (!validLocations.contains(transaction.getLocation())) 
+        {
+            transaction.setf(true);
+            System.out.println("Geographical Check:" + transaction.getAccountId());
+        } 
+        else if (next_Check != null) 
+        {
+            next_Check.check(transaction);
         }
     }
 }
