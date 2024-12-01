be03c4bce12772f381f1ffc960fbb450b8593954
rajuahmed118124 <rajuahmed118124@gmail.com>
2024-12-01 11:54:39 +0600

    "3rd Commit"

--- Scenario alpha/Main.java
+++ Scenario alpha/Main.java
@@ -1,6 +1,83 @@
 import java.util.Arrays;
 import java.util.List;
 
+public class BlacklistedMerchantCheck implements Fraud_Check {
+    private Fraud_Check next_Check;
+    private List<String> blacklistedMerchants = Arrays.asList("STORE", "MM");
+
+    @Override
+    public void setnext_Check(Fraud_Check next_Check) 
+    {
+        this.next_Check = next_Check;
+    }
+
+    @Override
+    public void check(Transaction transaction) 
+    {
+        if (blacklistedMerchants.contains(transaction.getMerchant())) 
+        {
+            transaction.setFlagged(true);
+            System.out.println("Check: " + transaction.getAccountId());
+        } 
+        else if (next_Check != null) 
+        {
+            next_Check.check(transaction);
+        }
+    }
+}
+
+public class HighRiskTransactionCheck implements Fraud_Check {
+    private Fraud_Check next_Check;
+
+    @Override
+    public void setnext_Check(Fraud_Check next_Check) 
+    {
+        this.next_Check = next_Check;
+    }
+
+    @Override
+    public void check(Transaction transaction) 
+    {
+        if (transaction.getAmount() > 5000 && transaction.getMerchant().equals("Online Betting")) {
+            transaction.setFlagged(true);
+            System.out.println("High-Risk Transaction Check Failed: High risk transaction for " + transaction.getAccountId());
+        } 
+        else if (next_Check != null) 
+        {
+            next_Check.check(transaction);
+        }
+    }
+}
+
+public class FraudDetectionSystem {
+    private Fraud_Check start_Check;
+
+    public FraudDetectionSystem() 
+    {
+        Fraud_Check baseCheck = new BaseFraudCheck();
+        Fraud_Check geoCheck = new GeographicalAnomalyCheck();
+        Fraud_Check blacklistCheck = new BlacklistedMerchantCheck();
+        Fraud_Check highRiskCheck = new HighRiskTransactionCheck();
+
+        baseCheck.setnext_Check(geoCheck);
+        geoCheck.setnext_Check(blacklistCheck);
+        blacklistCheck.setnext_Check(highRiskCheck);
+
+        this.start_Check = baseCheck;
+    }
+
+    public void evaluateTransaction(Transaction transaction) 
+    {
+        start_Check.check(transaction);
+        if (transaction.isFlagged()) 
+        {
+            System.out.println("Transaction flagged!");
+        } 
+        else {
+            System.out.println("Yes. Approved!");
+        }
+    }
+}
 
 public class Main {
     public static void main(String[] args) 
