09d73447a46c1912c6f4812a4efbd12c58557c5e
rajuahmed118124 <rajuahmed118124@gmail.com>
2024-12-01 12:50:13 +0600

    "First commit"

---
+++ Scenario beta/CSVProcessor.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class CSVProcessor implements FileProcessor {
+    @Override
+    public void process(String filePath) throws Exception {
+        List<String> lines = Files.readAllLines(Paths.get(filePath));
+        for (String line : lines) 
+        {
+            String[] values = line.split(",");
+            System.out.println(Arrays.toString(values)); 
+        }
+    }
+}
